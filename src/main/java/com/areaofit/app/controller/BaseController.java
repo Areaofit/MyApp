c5043a9619523b9be4933798efc0534ad07002fa
Areaofit <areaofit@qq.com>
2017-11-16 23:56:13 +0800

    web.xml 配置

---
+++ src/main/java/com/areaofit/app/controller/BaseController.java
@@ -0,0 +1,15 @@
+package com.areaofit.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/app")
+public class BaseController {
+
+	@RequestMapping("test")
+	public void test(){
+		System.out.println("ok");
+	}
+	
+}
