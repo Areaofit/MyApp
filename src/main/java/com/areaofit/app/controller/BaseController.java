62b21c5cdf89ef4b21b2bf7e2bd324137a0e9763
Areaofit <areaofit@qq.com>
2017-11-17 10:53:50 +0800

    spring mvc 基本配置完成

--- src/main/java/com/areaofit/app/controller/BaseController.java
+++ src/main/java/com/areaofit/app/controller/BaseController.java
@@ -1,13 +1,20 @@
 package com.areaofit.app.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.areaofit.app.dao.UserDao;
 
 @Controller
 @RequestMapping("/app")
 public class BaseController {
+	
+	@Resource
+	private UserDao userDao;
 
-	@RequestMapping("test")
+	@RequestMapping("/test")
 	public void test(){
 		System.out.println("ok");
 	}
--- src/main/java/com/areaofit/app/dao/UserDao.java
+++ src/main/java/com/areaofit/app/dao/UserDao.java
@@ -1,5 +1,12 @@
 package com.areaofit.app.dao;
 
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+@Component
 public interface UserDao {
+	
+	public Map<String, Object> getUserById(String id);
 
 }
